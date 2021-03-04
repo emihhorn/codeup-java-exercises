d5d94993a7c34182a63f3246f0c202ffa570cdd2
emihhorn <emilyhopehorn@gmail.com>
2021-03-04 14:19:39 -0600

    ce.java

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,73 +1,87 @@
 import java.util.Scanner;
 
 public class ConsoleExercises {
-//    public class ConsoleIOLecture {
 
-        public static void main(String[] args) {
-            double pi = 3.14159;
-            System.out.printf("\n The value of pi is approximately %.2f. \n", pi);
-            // ================================== print() and println()
+    public static void main(String[] args) {
 
 
-            int age = 23;
-            String name = "Fred";
-            String state = "Texas";
-            System.out.printf("%s is from %s!", name, state);
+//Explore the Scanner Class.
+//Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        String first = sc.next();
+        String second = sc.next();
+        String third = sc.next();
 
-// Fred is from Texas and is 23 years old
+        System.out.println(first + second + third);
+            System.out.print("Please enter an integer: ");
+            int userInt = sc.nextInt();
+            System.out.println(userInt);
 
-
-            // ================================== USER INPUT
-   /* TODO: use the scanner to take in the name of the month you were born
-                 and print it back out
-            - remember to first prompt the user for the input
-         */
-//        Scanner sc = new Scanner(System.in);
-            Scanner sc = new Scanner(System.in);
-
-            System.out.println("Please enter month you were born");
-            String month = sc.next();
-            System.out.println("You were born in " + month);
-
-//            sc.nextLine();
-//            String month = sc.nextLine();
-
-
-/* TODO: use the scanner to take in your birthday as an integer and then name of the month you were born
-    and print it back out.
-            - remember to first prompt the user for the birthday day of the month and store as an integer
-            - next take in name of the month with .nextLine()
-            - print out the result
-            - use a fix to prevent the month name input from being skipped
-         */
-            // .next() captures each input separated by a string:
-//            System.out.print("Please enter your first, middle, and last name: ");
-//            String firstName = sc.next();
-//            String lastName = sc.next();
-//            String what = sc.next();
+//What happens if you input something that is not an integer?
+       // input mismatch exception
+//Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them back in the console, each on a newline.
 //
-//            System.out.println(firstName);
-//            System.out.println(lastName);
-//            System.out.println(what);
 
-            // .nextInt() captures the first valid int value:
-//            System.out.print("Please enter your age: ");
-//            int age = sc.nextInt();
-//            System.out.println(age);
+        System.out.print("Enter 3 words: ");
+             String word1 = sc.next();
+             String word2 = sc.next();
+             String word3 = sc.next();
+             System.out.println(word1);
+             System.out.println(word2);
+             System.out.println(word3);
 
-            // .nextLine():
-//            System.out.print("Favorite quote: ");
-//            String quote = sc.nextLine();
-//            System.out.println(quote);
+        //        // What happens if you enter less than 3 words?
+////             It won't let you continue with out finishing the inputs
+//
+//        //  What happens if you enter more than 3 words?
+////             It will take only the first three inputs
+//
+//        // Prompt a user to enter a sentence, then store that sentence in a String variable using the .next method, then display that sentence back to the user.
+             System.out.println("Please enter a sentence:");
+             String sentence = sc.next();
+             System.out.println(sentence);
 
-            // Quirk of using next() then nextLine()...
-            // https://stackoverflow.com/questions/13102045/scanner-is-skipping-nextline-after-using-next-or-nextfoo
-            // https://coderanch.com/wiki/676482/Scanner-nextLine-gotcha
+//        // do you capture all of the words?
+//            // no
+//
+//        // Rewrite the above example using the .nextLine method.
+//
+//            System.out.println("Please enter a sentence:");
+//            String sentence = sc.nextLine();
+//            System.out.println(sentence);
+//
+//        // Calculate the perimeter and area of Codeup's classrooms. Prompt the user to enter values of length and width of the class. Use the .nextLine method to get user input and cast the resulting string to a numeric type.
+//
+//            System.out.println("Enter the length: ");
+//            String lengthInput = sc.nextLine();
+//            System.out.println("Enter the width: ");
+//            String widthInput = sc.nextLine();
+
+//
+//            double length = Double.parseDouble(lengthInput);
+//            double width = Double.parseDouble(widthInput);
+//            System.out.println("The area is: " + (length * width) + ".");
+//            System.out.println("The perimeter is: " + (length * 2 + width * 2) + ".");
+//
+//
+//        // Assume that the rooms are perfect rectangles.
+//
+//
+//
+//        // Assume that the user will enter valid numeric data for length and width.
+//        // Display the area and perimeter of that classroom.
+//
+//        // area = length x width
+//        // perimeter = 2 x length + 2 x width
+//
+//        // Bonus
+//
+//        // Accept decimal entries
+//        // Calculate the volume of the rooms
 
 
-
-
-        }
+    }
 
     }
 
