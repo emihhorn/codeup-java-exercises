0b98ed39a137772ae4612b084b4f9da580055db5
emihhorn <emilyhopehorn@gmail.com>
2021-03-04 13:51:50 -0600

    pract

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,52 +1,45 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
 //    public class ConsoleIOLecture {
 
         public static void main(String[] args) {
-
+            double pi = 3.14159;
+            System.out.printf("\n The value of pi is approximately %.2f. \n", pi);
             // ================================== print() and println()
 
-            // this:
-//            System.out.println("here");
-//            System.out.println("there");
 
-            // is equivalent to this:
-//            System.out.print("here\n");
-//            System.out.print("there\n");
+            int age = 23;
+            String name = "Fred";
+            String state = "Texas";
+            System.out.printf("%s is from %s!", name, state);
 
-            // without the newline characters, print outputs this:
-//            System.out.print("here");
-//            System.out.print("there");
-
-            // to concatenate, just like JS:
-//            System.out.println("Hello" + " " + "World");
-
-            // ================================== printf() / .format()
-
-            // Print a formatted string using the following... printf(formatString, data)
-
-            // same output as print():
-//            System.out.printf("Hello");
-//            System.out.printf("World!");
-
-            // creating a string variable:
-            String bigBall = "World";
-            System.out.printf("Hello %s", bigBall);
-            System.out.printf("Hello %S", bigBall); // all caps
-
-            // multiple variables:
-//            int three = 3;
-//            String typeOfPetGroup = "cats";
-//            System.out.printf("I have %d %s.", three, typeOfPetGroup);
-
-            // currency:
-//            int currencyPennies = 1431;
-//            System.out.printf("I'll sell you swamp land for $%.2f an acre!", currencyPennies / 100.00);
+// Fred is from Texas and is 23 years old
 
 
             // ================================== USER INPUT
-
+   /* TODO: use the scanner to take in the name of the month you were born
+                 and print it back out
+            - remember to first prompt the user for the input
+         */
 //        Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
 
+            System.out.println("Please enter month you were born");
+            String month = sc.next();
+            System.out.println("You were born in " + month);
+
+//            sc.nextLine();
+//            String month = sc.nextLine();
+
+
+/* TODO: use the scanner to take in your birthday as an integer and then name of the month you were born
+    and print it back out.
+            - remember to first prompt the user for the birthday day of the month and store as an integer
+            - next take in name of the month with .nextLine()
+            - print out the result
+            - use a fix to prevent the month name input from being skipped
+         */
             // .next() captures each input separated by a string:
 //            System.out.print("Please enter your first, middle, and last name: ");
 //            String firstName = sc.next();
@@ -70,13 +63,9 @@ public class ConsoleExercises {
             // Quirk of using next() then nextLine()...
             // https://stackoverflow.com/questions/13102045/scanner-is-skipping-nextline-after-using-next-or-nextfoo
             // https://coderanch.com/wiki/676482/Scanner-nextLine-gotcha
-//            Scanner sc = new Scanner(System.in);
-//            System.out.print("Please enter your favorite number: ");
-//            int num = sc.nextInt();
-//            System.out.println(num);
-//            System.out.print("Please enter your favorite words: ");
-//            String words = sc.nextLine();
-//            System.out.println(words);
+
+
+
 
         }
 
