0e40dff0790567d5b00abfe59e4069400ab658fb
emihhorn <emilyhopehorn@gmail.com>
2021-03-04 09:31:53 -0600

    java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -15,16 +15,16 @@ public class HelloWorld {
 //        String myString = 3.14159; //incompatible types
 //        System.out.println(myString);
 //    5.Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
-            long myNumber;
-            System.out.println(myNumber);// not initialized
+//            long myNumber;
+//            System.out.println(myNumber);// not initialized
 //    6.Change your code to assign the value 3.14 to myNumber. What do you notice?
 //        myNumber = 3.14// incompatible types
 //    7.Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
-          myNumber =123L;
-          System.out.println(myNumber);
+//          myNumber =123L;
+//          System.out.println(myNumber);
 //    8.Change your code to assign the value 123 to myNumber.
-        myNumber = 123;
-        System.out.println(myNumber);
+//        myNumber = 123;
+//        System.out.println(myNumber);
 
 //    9.Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
 
@@ -37,5 +37,51 @@ public class HelloWorld {
 
 
 
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+
+//Try to create a variable named class. What happens?
+//
+//Object is the most generic type in Java. You can assign any value to a variable of type Object.
+// What do you think will happen when the following code is run?
+
+ String theNumberThree = "three";
+ Object o = theNumberThree;
+ int three = (int) o;
+//error @ runtime
+//Copy and paste the code above and then run it. Does the result match with your
+// expectation?
+//   How is the above example different from the code block below?
+
+//        int three = (int) "three";
+
+
+//Rewrite the following expressions using the relevant shorthand assignment operators:
+
+//        int x = 4;
+//        x = x + 5;
+
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+
+
+ //       What happens if you assign a value to a numerical variable that is larger
+        //       (or smaller) than the type can hold? What happens if you increment a numeric variable past the type's capacity?
+
+  //      Hint: Integer.MAX_VALUE is a class constant (we'll learn more about these later)
+        //      that holds the maximum value for the int type.
     }
 }
