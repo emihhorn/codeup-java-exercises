e5ddf0cc99e7ba0953e54c7ef1f43f16a1334b11
emihhorn <emilyhopehorn@gmail.com>
2021-03-05 14:09:34 -0600

    StringEx

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class StringExercise {
+import java.util.Scanner;
+public class StringExercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    //For each of the following output examples:
+    // create a String variable that contains the
+    // desired output and print it out to the console,
+    // you can do this with only one String variable and
+    // one print statement for each output example.
+
+    //We don't need no education
+//    String string1 = "We don't need no education\nWe don't need no thought control";
+//    System.out.println(string1);
+
+    //We don't need no thought control
+//    String string2 = "We don't need no thought control";
+//    System.out.println(string2);
+
+
+//    //Check "this" out!, "s inside of "s!
+
+        String message2 = "Check \"this\" out!, \"s inside of \"s!\n";
+        System.out.println(message2);
+        String message3 = "in windows, the main drive is usually C:\\\n";
+        System.out.println(message3);
+        String message4 = "I can do backslashes \\, double backslashes \\\\, \nand the amazing triple backslash \\\\\\!";
+        System.out.println(message4);
+
+        //In windows, the main drive is usually C:\
+    //
+    //I can do backslashes \, double backslashes \\,
+    //and the amazing triple backslash \\\!
+
+}
 }
