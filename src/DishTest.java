533016ea82823bb16bf03de09b7e5fc3e0c5522a
emihhorn <emilyhopehorn@gmail.com>
2021-03-10 17:31:35 -0600

    Dtest

--- src/DishTest.java
+++ src/DishTest.java
@@ -2,15 +2,15 @@ public class DishTest {
     public static void main(String[] args){
 
         Dish dish1 = new Dish();
-        dish1.costInCents = 2500;
-        dish1.nameOfDish = "Soup";
-        dish1.wouldRecommend = true;
+//        dish1.costInCents = 2500;
+//        dish1.nameOfDish = "Soup";
+//        dish1.wouldRecommend = true;
 
 
         Dish dish2 = new Dish();
-        dish2.nameOfDish = "Tofu Burger";
-        dish2.costInCents = 1100;
-        dish2.wouldRecommend = true;
+//        dish2.nameOfDish = "Tofu Burger";
+//        dish2.costInCents = 1100;
+//        dish2.wouldRecommend = true;
 
 //        dish1.printSummary();
 
