79541ae7a13798e2779835519f46ca66ee656e62
emihhorn <emilyhopehorn@gmail.com>
2021-03-10 17:32:22 -0600

    Person

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    private String name;
+
+    public String getName(){
+//TODO: return the person's name
+        return name;
+    }
+
+    public void setName(String name){
+//TODO: change the name property to the passed value
+    this.name = name;
+    }
+
+
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+        System.out.println("Hello" + name);
+    }
 }
