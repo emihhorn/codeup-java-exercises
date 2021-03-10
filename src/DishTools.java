0b54ebf563374ded13852a7a5ccf8d033cf50fcb
emihhorn <emilyhopehorn@gmail.com>
2021-03-10 10:31:19 -0600

    dishTools

--- src/DishTools.java
+++ src/DishTools.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class DishTools {
+import java.util.Locale;
+
+public class DishTools {
+    public static final int averageCost = 1300;
+
+    public static final String shoutDishName(Dish a){
+        return a.nameOfDish.toUpperCase();
+    }
+
+
+    public static final String analyzeDishCost(Dish a){
+        if (a.costInCents > averageCost) {
+            return "More expensive than average";
+        }else  if (a.costInCents < averageCost) {
+            return "Less expensive than average";
+        }else{
+            return "That's average";
+        }
+    }
+
+
 }
