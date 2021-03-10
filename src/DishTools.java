26c89059c513e54b05f586b6fe26adfccce3c1e3
emihhorn <emilyhopehorn@gmail.com>
2021-03-10 17:32:00 -0600

    DTools

--- src/DishTools.java
+++ src/DishTools.java
@@ -1,28 +1,28 @@
 public class DishTools {
     public static final int averageCost = 1300;
 
-    public static final String shoutDishName(Dish food){
-        return food.nameOfDish.toUpperCase();
+//    public static final String shoutDishName(Dish food){
+//        return food.nameOfDish.toUpperCase();
     }
 
+//
+//    public static final String analyzeDishCost(Dish food){
+//        if (food.costInCents > averageCost) {
+//            return "More expensive than average";
+//        }else  if (food.costInCents < averageCost) {
+//            return "Less expensive than average";
+//        }else{
+//            return "That's average";
+//        }
+//    }
 
-    public static final String analyzeDishCost(Dish food){
-        if (food.costInCents > averageCost) {
-            return "More expensive than average";
-        }else  if (food.costInCents < averageCost) {
-            return "Less expensive than average";
-        }else{
-            return "That's average";
-        }
-    }
-
-    public static boolean flipRecommendation(Dish food) {
-        food.wouldRecommend = !food.wouldRecommend;
-        System.out.println(food.wouldRecommend);
-        return false;
-    }
-
-
-
-
-}
+//    public static boolean flipRecommendation(Dish food) {
+//        food.wouldRecommend = !food.wouldRecommend;
+//        System.out.println(food.wouldRecommend);
+//        return false;
+//    }
+//
+//
+//
+//
+//}
