6b06a362ae9f5a312dbe0ae08c8a38e5c13b44e7
emihhorn <emilyhopehorn@gmail.com>
2021-03-10 10:30:45 -0600

    dishTest

--- src/DishTest.java
+++ src/DishTest.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class DishTest {
+public class DishTest {
+    public static void main(String[] args){
+
+        Dish dish1 = new Dish();
+        dish1.costInCents = 2500;
+        dish1.nameOfDish = "Soup";
+        dish1.wouldRecommend = true;
+
+//        dish1.printSummary();
+
+
+        System.out.println(DishTools.shoutDishName(dish1) + "\n");
+        System.out.println(DishTools.analyzeDishCost(dish1) + "\n");
+    }
 }
