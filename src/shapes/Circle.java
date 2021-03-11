5c6a08e75142b45843d00a646645adb7be250c32
emihhorn <emilyhopehorn@gmail.com>
2021-03-11 11:32:33 -0600

    circles

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,2 +1,17 @@
-package shapes;public class Circle {
+package shapes;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        //pi x (radius ^ 2);
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+//    public double getCircumference()
+
+
 }
