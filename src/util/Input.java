9415592085af8f416e6fb23bcb41cb90498005ca
emihhorn <emilyhopehorn@gmail.com>
2021-03-11 11:33:39 -0600

    util

--- src/util/Input.java
+++ src/util/Input.java
@@ -14,10 +14,56 @@ public class Input {
         return scanner.nextLine();
     }
 
+    public boolean yesNo() {
+        String input = scanner.next();
+//        if (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes")) {
+//            return true;
+//        }else{
+//            return false;
+//        }
+        return (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes"));
+    }
+
+    public double getDouble() {
+        if(scanner.hasNextDouble()) {
+            return scanner.nextDouble();
+        }else{
+            System.out.println("Invalid, try again");
+            scanner.nextLine();
+            return getDouble();//recursion
+        }
+    }
+
+    public double getDouble(double min, double max) {
+        double userDouble = getDouble();
+        if (userDouble >= min && userDouble <= max) {
+            return userDouble;
+        }else{
+            System.out.println("out of range");
+            return getDouble(min, max);
+        }
+    }
 
     public static void main(String[] args) {
         Input input = new Input();
-        String userInput = input.getString();
-        System.out.println(userInput);
+        System.out.println("enter number btw 1-3.6");
+        double userDouble = input.getDouble(1, 3.6);
+        System.out.println("You entered: " + userDouble);
+
+
+//        int userInt = input.getInt();
+//        System.out.println("Your int is:" + userInt);
+//
+
+//        boolean willCont = input.yesNo();
+//        if (willCont) {
+//            System.out.println("Continuing...");
+//        } else {
+//            System.out.println("Exiting the matrix");
+//        }
+
+
+//        String userInput = input.getString();
+//        System.out.println(userInput);
     }
 }
