43ddbb38aee740ad27c89d6587dedd684fee3380
emihhorn <emilyhopehorn@gmail.com>
2021-03-12 10:48:39 -0600

    arraylecex

--- src/ArraysLecture/ArraysExercises.java
+++ src/ArraysLecture/ArraysExercises.java
@@ -6,6 +6,9 @@ import java.util.Arrays;
 
 public class ArraysExercises {
 
+//
+
+
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
@@ -16,14 +19,28 @@ public class ArraysExercises {
         peeps[1] = new Person("Luke");
         peeps[2] = new Person("Madeline");
 
+
+        peeps = addPerson(peeps, new Person("Landon"));
+
+
 //            String[] Persons = {"John", "Paul", "Ringo"};
 //    for (int i = 0; i < Persons.length; i += 1) {
 //        System.out.println(Persons[i]);
 //    }
 
+
         for (int i = 0; i < peeps.length; i+=1) {
             System.out.println(peeps[i].getName());
         }
+
+    }
+
+    public static Person[] addPerson(Person[] people, Person peeps){
+
+        people = Arrays.copyOf(people, people.length+1);
+        people[people.length - 1] = peeps;
+        return people;
+
 
     }
 }
