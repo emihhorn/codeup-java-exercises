0f0f174b586c22bc647583d4ee4e9d1d6a6bf7ff
emihhorn <emilyhopehorn@gmail.com>
2021-03-15 16:28:21 -0500

    square

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,2 +1,17 @@
-package shapes;public class Square {
+package shapes;
+
+public class Square extends Rectangle {
+
+    public Square(int side) {
+        super(side, side);
+        this.side = side;
+    }
+
+    public int getArea() {
+        return side * side;
+    }
+
+    public int getPerimeter() {
+        return 4 * side;
+    }
 }
