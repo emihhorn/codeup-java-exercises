589db0ab8832444277f849bd376bf6c730b2d222
emihhorn <emilyhopehorn@gmail.com>
2021-03-15 14:40:08 -0500

    pbj

--- src/jelly/PeanutButter.java
+++ src/jelly/PeanutButter.java
@@ -1,2 +1,13 @@
-package jelly;public class PeanutButter {
+package jelly;
+
+public class PeanutButter extends Ingredient{
+
+    public boolean isSmooth;
+
+    public String isBrandName;
+
+    public void throwAway() {
+        System.out.println("Throwing away peanut butter...");
+    }
+
 }
