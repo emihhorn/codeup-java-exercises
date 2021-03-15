40169fe82a8cfe6ee2f6f244ca95e51f50952d84
emihhorn <emilyhopehorn@gmail.com>
2021-03-15 16:27:47 -0500

    ShapesTest

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,2 +1,29 @@
-package shapes;public class ShapesTest {
+package shapes;
+
+public class ShapesTest {
+
+    public static void main(String[] args) {
+
+
+//       create a variable of the type Rectangle named box1 and assign it a new
+//       instance of the Rectangle class with a width of 4 and a length of 5
+
+        Rectangle box1 = new Rectangle(4, 5);
+
+//       verify that the getPerimeter and getArea methods return 18 and 20, respectively.
+        System.out.println(box1.getPerimeter());
+
+        System.out.println(box1.getArea());
+
+//        create a variable of the type Rectangle named box2 and assign it a new instance
+//        of the Square class that has a side value of 5.
+        Rectangle box2 = new Square(5);
+
+//       verify that the getPerimeter and getArea methods return 20 and 25, respectively
+        System.out.println(box2.getPerimeter());
+
+        System.out.println(box2.getArea());
+
+
+    }
 }
