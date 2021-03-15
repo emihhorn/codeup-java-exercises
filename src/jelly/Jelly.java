bf487fc22b41beaa910677c543910e4cde24b1a2
emihhorn <emilyhopehorn@gmail.com>
2021-03-15 14:37:58 -0500

    jelly

--- src/jelly/Jelly.java
+++ src/jelly/Jelly.java
@@ -1,2 +1,25 @@
-package jelly;public class Jelly {
+package jelly;
+
+public class Jelly extends Ingredient{
+
+    private int calories;
+
+    Jelly() {
+        super();
+    }
+
+    public void throwAway() {
+        System.out.println("Throwing away jelly...");
+    }
+
+//    public void throwAway() {
+//        System.out.println("Throwing away jelly...");
+//    }
 }
+
+
+
+//- override the method, throwAway, in your Jelly,
+// PeanutButter, and Bread classes to sout out a
+// more specific
+// message (e.g. "Throwing away Jelly...", etc.)
