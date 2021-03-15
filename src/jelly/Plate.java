547780408781dbd0bf8928a035287e94b7aac866
emihhorn <emilyhopehorn@gmail.com>
2021-03-15 14:40:37 -0500

    plate

--- src/jelly/Plate.java
+++ src/jelly/Plate.java
@@ -1,2 +1,14 @@
-package jelly;public class Plate {
+package jelly;
+
+public class Plate extends KitchenTool{
+   public String yourPlate;
+
+   public void putAway(){
+       System.out.println("Put your dishes up!");
+   }
+
+    public void wash() {
+        System.out.println("Washing dishes...");
+    }
+
 }
