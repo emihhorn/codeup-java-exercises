126c9677d5bee4354302564dcf875c6ceebc65ac
emihhorn <emilyhopehorn@gmail.com>
2021-03-15 14:39:03 -0500

    Ktools

--- src/jelly/KitchenTool.java
+++ src/jelly/KitchenTool.java
@@ -1,2 +1,28 @@
-package jelly;public class KitchenTool {
+package jelly;
+
+public class KitchenTool {
+
+    KitchenTool() {
+
+    }
+
+    public Boolean isClean() {
+        boolean clean = true;
+        boolean dirty = false;
+        if (clean) {
+            System.out.println("it's clean");
+            return true;
+        } if (dirty) {
+            System.out.println("That's dirty, do the dishes!");
+        }
+        return false;
+    }
+
+
+    public Boolean isDis(){
+        return false;
+    }
+
+    //    Knife (should extend from KitchenTool)
+    //    Plate (should extend from KitchenTool)
 }
