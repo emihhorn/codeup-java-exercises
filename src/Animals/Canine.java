f1d7b1e1aacac5d5edb13ecdf74e8da7c4630698
emihhorn <emilyhopehorn@gmail.com>
2021-03-16 11:09:34 -0500

    canine

--- src/Animals/Canine.java
+++ src/Animals/Canine.java
@@ -1,2 +1,37 @@
-package Animals;public class Canine {
+package Animals;
+
+public abstract class Canine implements Describable{
+
+    protected final String size;
+    protected final String commonName;
+    protected final String subSpecies;
+    protected final String sound;
+
+    public Canine(String size, String commonName, String sound, String subSpecies){
+        this.size = size;
+        this.commonName = commonName;
+        this.subSpecies = subSpecies;
+        this.sound = sound;
+    }
+
+    public abstract void printEmote();
+
+    public void makeSound() {
+        System.out.println(sound + "\n");
+    }
+
+    public void printDelimiter(){
+        System.out.println();
+    }
+    public void printSubSpecies(){
+        System.out.printf("Sub-Species: %s\n", this.subSpecies);
+    }
+    public void printCommonName(){
+        System.out.printf("Common Name: %s\n", this.commonName);
+    }
+    public void printSize(){
+        System.out.printf("Size: %s\n", this.size);
+    }
+
+
 }
