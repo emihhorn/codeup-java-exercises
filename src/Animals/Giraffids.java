9ef3802d7ba6cf73608e88aeaefd4a2ff9f51d26
emihhorn <emilyhopehorn@gmail.com>
2021-03-16 14:56:46 -0500

    FamilyOf

--- src/Animals/Giraffids.java
+++ src/Animals/Giraffids.java
@@ -1,2 +1,34 @@
-package Animals;public class Giraffids {
+package Animals;
+
+public abstract class Giraffids implements Describable {
+
+    protected final String height;
+
+    protected final String UnguligradeSpecies;
+    protected final String sound;
+
+    public Giraffids(String height, String sound, String UnguligradeSpecies){
+        this.height = height;
+
+        this.UnguligradeSpecies = UnguligradeSpecies;
+        this.sound = sound;
+    }
+
+    public abstract void printEmote();
+
+    public void makeSound() {
+        System.out.println(sound + "\n");
+    }
+
+    public void printDelimiter(){
+        System.out.println();
+    }
+    public void printUnSpecies(){
+        System.out.printf("Unguligrade-Species: %s\n", this.UnguligradeSpecies);
+    }
+
+    public void printSize(){
+        System.out.printf("Size: %s\n", this.height);
+    }
+
 }
