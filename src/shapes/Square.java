aa9611254b91d729dc2ad9ac0cc650e6150b5adf
emihhorn <emilyhopehorn@gmail.com>
2021-03-17 08:46:48 -0500

    square

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,17 +1,31 @@
 package shapes;
 
-public class Square extends Rectangle {
+public class Square extends Quadrilateral {
 
-    public Square(int side) {
+    public Square(double side) {
         super(side, side);
-        this.side = side;
     }
 
-    public int getArea() {
-        return side * side;
+    @Override
+    public void setLength(double length){
+        this.length = length;
     }
 
-    public int getPerimeter() {
-        return 4 * side;
+    @Override
+    public void setWidth(double width) {
+        this.width = width;
     }
+
+//    public Square(int side) {
+//        super(side, side);
+//        this.side = side;
+//    }
+//
+//    public int getArea() {
+//        return side * side;
+//    }
+//
+//    public int getPerimeter() {
+//        return 4 * side;
+//    }
 }
