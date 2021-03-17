ddebcb21cf06f684c59e83b686d23ef6b361b666
emihhorn <emilyhopehorn@gmail.com>
2021-03-17 08:44:41 -0500

    shapes

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -1,2 +1,32 @@
-package shapes;public class Quadrilateral {
+package shapes;
+
+
+public abstract class Quadrilateral implements Shape {
+
+    //TODO: Inside of shapes, create
+    // an abstract Quadrilateral class that implements Shape.
+    // This class should have -> protected double fields for length
+    // and width. ***include data type***-> a constructor that accepts
+    // two doubles for the length and width and sets those fields.
+    //    -> abstract methods for setting the length and width.
+
+
+    protected double length;
+    protected double width;
+
+    public Quadrilateral(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public abstract void setLength(double length);
+
+    public abstract void setWidth(double width);
+
+    public double getArea(){
+       return(this.length * this.width);
+    }
+    public double getPerimeter(){
+       return ((this.length * 2) + (this.width * 2));
+    }
 }
