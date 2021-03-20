fb0d69d0b7850aefe8de7f741a2f1e72e2f9f92d
emihhorn <emilyhopehorn@gmail.com>
2021-03-19 22:17:18 -0500

    inputT

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,2 +1,24 @@
-package util;public class InputTest {
+package util;
+
+public class InputTest {
+    public static void main(String[] args) {
+//        var input = new Input();
+//        System.out.println("enter an integer");
+//       try {
+//           System.out.println(input.getInt());
+//       }catch (NumberFormatException n){
+//           System.out.println(n.getMessage());
+//           System.out.println("Please enter an integer");
+//       }finally{
+//           System.out.println("The try catch is finished");
+//       }
+        var input1 = new Input();
+        System.out.println("enter an integer");
+        try {
+            System.out.println(input1.getInt());
+        } catch (NumberFormatException e) {
+            System.out.println("Not valid");
+            System.out.println(e.getMessage());
+        }
+    }
 }
