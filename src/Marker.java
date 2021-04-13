ba7379e252733fee0b67c59128d1df7c77107857
emihhorn <emilyhopehorn@gmail.com>
2021-04-12 23:17:45 -0500

    marker

--- src/Marker.java
+++ src/Marker.java
@@ -1,21 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
-
-
-
-public class quiz {
-
-
-}
-
-
-
 public class Marker {
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(13);
-        numbers.add(new Integer(8)); // Line 7
-        numbers.add(3.6); // Line 8
-        for (Integer number: numbers) System.out.println(number); // Line 9
-    }
+
+
+        String[] names = {"Rick", "Stacey", "Brad", "Becca"};
+        String x;
+        try {
+            x = names[100];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception caught!");
+            x = "";
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("This will always run.");
+        }
+
+        }
 }
+
+
+
