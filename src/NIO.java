147f9063bc954ede7d5bd47c94626e76e38730b0
emihhorn <emilyhopehorn@gmail.com>
2021-04-19 22:11:24 -0500

    the string uses only lowercase a-z

--- src/NIO.java
+++ src/NIO.java
@@ -51,7 +51,23 @@ public class NIO {
         }
         return true;
     }
+
+    // Reduce space usage by a factor of eight by using a bit vector.
+    boolean isUniqueChar(String str) {
+        int checker = 0;
+        for (int i = 0; i < str.length(); i++) {
+            int val = str.charAt(i) - 'a';
+            if ((checker & (1 << val)) > 0) {
+                return false;
+            }
+            checker |= (1 << val);
+        }
+        return true;
+    }
+
+
 }
+
 
 
 
