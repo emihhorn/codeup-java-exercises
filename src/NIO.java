4b289f5854b1ab2e9f3f378689a4a4e32f9ebca4
emihhorn <emilyhopehorn@gmail.com>
2021-04-19 22:27:50 -0500

    permutation is case senstive and whitespace is significant

--- src/NIO.java
+++ src/NIO.java
@@ -65,7 +65,18 @@ public class NIO {
         return true;
     }
 
+// Given two strings, write a method to decide if one is a permutation of the other.
 
+    String sort(String s) {
+        char[] content = s.toCharArray();
+        java.util.Arrays.sort(content);
+        return new String(content);
+    }
+    boolean permutation(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+    }
 }
 
 
